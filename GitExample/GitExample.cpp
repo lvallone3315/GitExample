73592c8a54f037f8dae76f85a50b32940a8e0f22
Lee Vallone <lvallone3315@gmail.com>
2021-04-01 16:23:27 -0400

    Adding yet another cout

--- GitExample/GitExample.cpp
+++ GitExample/GitExample.cpp
@@ -3,10 +3,13 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "  Hello from GitExample_branch1!\n\n\n";
+    cout << "Hello using cout shorthand\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
